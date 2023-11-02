8c04f1dc3a8f7520a6cf7ebe77c0f909a8dde82d
RegisLewis <regisaslewis@gmail.com>
2023-11-02 13:33:59 -0700

    cookies and sessions lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,29 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    articles = Article.query.all()
+    article_list = []
+    for n in articles:
+        article_list.append(n.to_dict())
 
-    pass
+    return make_response(article_list, 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    article = Article.query.filter_by(id = id).first()
+    session["page_views"] = session.get("page_views") or 0
+    session["page_views"] += 1
+    if article and session["page_views"] <= 3:
+        body = article.to_dict()
+        status = 200
+    elif article and session["page_views"] > 3:
+        body = {"message": "Maximum pageview limit reached"}
+        status = 401
+    else:
+        body = f"Article # {id} not found."
+        status = 404
+    
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555)
